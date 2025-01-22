1ef2b01fab6f94db9996de005c7ae43ad8b55d0b
SergeiVS <fam.smirnoff@gmail.com>
2025-01-22 18:06:40 +0100

    initial commit

---
+++ src/main/java/org/lets_play_be/LetsPlayBeApplication.java
@@ -0,0 +1,13 @@
+package org.lets_play_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsPlayBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsPlayBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lets_play_be/LetsPlayBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lets_play_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsPlayBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
