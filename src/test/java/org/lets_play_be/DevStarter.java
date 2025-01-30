27dbe86ee80fd8c9db58c949991198aeca9fb367
pavel <pavel.smirnov121@gmail.com>
2025-01-30 17:45:09 +0100

    dev starter setup

---
+++ src/test/java/org/lets_play_be/DevStarter.java
@@ -0,0 +1,12 @@
+package org.lets_play_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevStarter.class, args);
+    }
+}
