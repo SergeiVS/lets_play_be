c471a0f8a65f2745c018202494e38979192a362d
pavel <pavel.smirnov121@gmail.com>
2025-02-10 21:11:52 +0100

    clean code refactor

--- src/main/java/org/lets_play_be/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/org/lets_play_be/security/filter/JwtAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package org.lets_play_be.security.filter;
 
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.SignatureException;
@@ -31,66 +32,77 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
 
     @Override
-    protected void doFilterInternal(@NonNull HttpServletRequest request,
-                                    @NonNull HttpServletResponse response,
-                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
-
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
         try {
+            if (isRequestToSwagger(request)) return;
 
-            if (request.getRequestURI().startsWith("/swagger-ui") || request.getRequestURI().startsWith("/v3/api-docs")) {
-                filterChain.doFilter(request, response);
-                return;
-            }
-
-            String jwt = getAtJwtFromCookie(request);
-
-            if (jwt == null) {
-                filterChain.doFilter(request, response);
-                return;
-            }
-
-            String username = jwtService.getUsernameFromToken(jwt);
-
-            if (username != null) {
-                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-                if (jwtService.validateToken(jwt, userDetails) && SecurityContextHolder.getContext().getAuthentication() == null) {
-                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                            userDetails, null, userDetails.getAuthorities());
-                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(authToken);
-                }
-            }
-        } catch (SignatureException e) {
-            log.error("Invalid jwt signature: {}", e.getMessage());
-            filterChain.doFilter(request, response);
-            return;
-        } catch (MalformedJwtException e) {
-            log.error("Invalid jwt token: {}", e.getMessage());
-            filterChain.doFilter(request, response);
-            return;
-        } catch (ExpiredJwtException e) {
-            log.error("Expired jwt token: {}", e.getMessage());
-            filterChain.doFilter(request, response);
-            return;
-        } catch (UnsupportedJwtException e) {
-            log.error("Unsupported jwt token: {}", e.getMessage());
-            filterChain.doFilter(request, response);
-            return;
+            handleFilterInternal(request);
+        } catch (JwtException e) {
+            logJwtException(e);
         } catch (IllegalArgumentException e) {
             log.error("Jwt token is empty: {}", e.getMessage());
+        } finally {
             filterChain.doFilter(request, response);
-            return;
         }
-        filterChain.doFilter(request, response);
+    }
+
+    private void handleFilterInternal(HttpServletRequest request) {
+        String jwt = getAtJwtFromCookie(request);
+
+        if (jwt == null) return;
+
+        String username = jwtService.getUsernameFromToken(jwt);
+
+        if (username == null) return;
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        if (isValidTokenAndNotAuthenticated(jwt, userDetails)) {
+            validateCredentials(request, userDetails);
+        }
+    }
+
+    private void validateCredentials(HttpServletRequest request, UserDetails userDetails) {
+        var authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+    }
+
+    private boolean isValidTokenAndNotAuthenticated(String jwt, UserDetails userDetails) {
+        return jwtService.validateToken(jwt, userDetails) && SecurityContextHolder.getContext().getAuthentication() == null;
+    }
+
+    private boolean isRequestToSwagger(HttpServletRequest request) {
+        return request.getRequestURI().startsWith("/swagger-ui") || request.getRequestURI().startsWith("/v3/api-docs");
+    }
+
+    private void logJwtException(JwtException e) {
+        log.error(determineLogMsg(e), e.getMessage());
+    }
+
+    private String determineLogMsg(JwtException e) {
+        return switch (e) {
+            case SignatureException ignored -> "Signature exception: {}";
+            case MalformedJwtException ignored -> "Malformed jwt token: {}";
+            case ExpiredJwtException ignored -> "Expired jwt token: {}";
+            case UnsupportedJwtException ignored -> "Unsupported jwt token: {}";
+            default -> throw new RuntimeException("Unexpected error at JwtAuth: {}");
+        };
     }
 
     private String getAtJwtFromCookie(HttpServletRequest request) {
-
         String jwt = null;
 
         if (request.getCookies() != null) {
             jwt = jwtService.getAccessTokenFromCookie(request);
         }
+
         return jwt;
     }
 }
