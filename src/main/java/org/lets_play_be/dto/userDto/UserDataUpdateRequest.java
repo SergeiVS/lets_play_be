77a9aad39bc0c8cd454bab5d89e3e6ba09c0cfe1
SergeiVS <fam.smirnoff@gmail.com>
2025-03-02 20:43:10 +0100

    Fix Double Annotation by UserDataUpdateRequest

--- src/main/java/org/lets_play_be/dto/userDto/UserDataUpdateRequest.java
+++ src/main/java/org/lets_play_be/dto/userDto/UserDataUpdateRequest.java
@@ -2,8 +2,7 @@ package org.lets_play_be.dto.userDto;
 
 import jakarta.validation.constraints.NotNull;
 
-public record UserDataUpdateRequest(@NotNull(message = "Id could not me null")
-                                    @NotNull(message = "UserId could not be null")
+public record UserDataUpdateRequest(@NotNull(message = "UserId could not be null")
                                     Long userId,
                                     String newName,
                                     String newAvatarUrl) {
