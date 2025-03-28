2816e8c662062bc69959faa1895c5007fba2debd
SergeiVS <fam.smirnoff@gmail.com>
2025-03-28 20:39:31 +0100

    add create LobbyActive, updateLobby time and title

--- src/test/java/org/lets_play_be/service/lobbyService/LobbyActiveCRUDServiceTest.java
+++ src/test/java/org/lets_play_be/service/lobbyService/LobbyActiveCRUDServiceTest.java
@@ -1,13 +1,25 @@
 package org.lets_play_be.service.lobbyService;
 
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.lets_play_be.dto.lobbyDto.ActiveLobbyResponse;
+import org.lets_play_be.dto.lobbyDto.NewActiveLobbyRequest;
+import org.lets_play_be.entity.lobby.LobbyActive;
+import org.lets_play_be.entity.user.AppUser;
+import org.lets_play_be.entity.user.UserAvailability;
+import org.lets_play_be.service.InviteService.InviteService;
 import org.lets_play_be.service.appUserService.AppUserService;
+import org.lets_play_be.service.mappers.LobbyMappers;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static com.fasterxml.jackson.databind.type.LogicalType.Collection;
 
 @ExtendWith(MockitoExtension.class)
 class LobbyActiveCRUDServiceTest {
@@ -17,10 +29,36 @@ class LobbyActiveCRUDServiceTest {
 
     @Mock
     AppUserService userService;
-    
+
+    @Mock
+    InviteService inviteService;
+
+    LobbyMappers lobbyMappers;
+
+    @InjectMocks
+    LobbyActiveCRUDService lobbyActiveCRUDService;
+
+    NewActiveLobbyRequest newLobbyRequest;
+
+    ActiveLobbyResponse lobbyResponse;
+
+    LobbyActive presaveLobby;
+
+    LobbyActive savedLobby;
+
+    AppUser owner;
+
+    AppUser user;
+
+    UserAvailability ownerAvailability;
+
+    UserAvailability userAvailability;
 
     @BeforeEach
     void setUp() {
+
+
+        newLobbyRequest = new NewActiveLobbyRequest("title", "18:00:00+00:00", "test message", new ArrayList<>());
 
     }
 
