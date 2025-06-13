11079a4457dad9bac69edf88b5bea25fdcf762b5
SergeiVS <fam.smirnoff@gmail.com>
2025-06-13 19:56:22 +0200

    setting up pom.xml and application-test.yaml

---
+++ src/test/java/org/lets_play_be/repository/AppUserRepositoryTest.java
@@ -0,0 +1,34 @@
+package org.lets_play_be.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppUserRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void existsByEmail() {
+    }
+
+    @Test
+    void existsByName() {
+    }
+
+    @Test
+    void findAppUserByEmail() {
+    }
+
+    @Test
+    void getAppUsersById() {
+    }
+}
