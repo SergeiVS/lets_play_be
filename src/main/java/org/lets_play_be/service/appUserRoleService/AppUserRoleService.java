c8983b10138a93ebfaa97fb6c24dfd962c74f3cc
SergeiVS <fam.smirnoff@gmail.com>
2025-06-14 10:55:08 +0200

    Added Test for AppUserRoleRepository

--- src/main/java/org/lets_play_be/service/appUserRoleService/AppUserRoleService.java
+++ src/main/java/org/lets_play_be/service/appUserRoleService/AppUserRoleService.java
@@ -1,19 +1,28 @@
 package org.lets_play_be.service.appUserRoleService;
 
 import lombok.RequiredArgsConstructor;
+import org.lets_play_be.entity.enums.UserRoleEnum;
 import org.lets_play_be.entity.user.AppUserRole;
 import org.lets_play_be.repository.AppUserRoleRepository;
+import org.lets_play_be.service.initialization.AppUserRoleMapping;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+import static org.lets_play_be.entity.enums.UserRoleEnum.findRole;
 
 @Service
 @RequiredArgsConstructor
 public class AppUserRoleService {
 
-    private final AppUserRoleRepository appUserRoleRepository;
+    private final AppUserRoleMapping mapping;
+
 
     public AppUserRole getRoleByNameOrThrow(String roleName) {
-       return appUserRoleRepository.findByNameIgnoreCase(roleName)
-               .orElseThrow(()-> new RuntimeException("Role with name " + roleName + " not found"));
+
+       UserRoleEnum role = findRole(roleName).orElseThrow(() -> new IllegalArgumentException("Invalid role name: " + roleName));
+
+       return mapping.get(role);
     }
 
 }
