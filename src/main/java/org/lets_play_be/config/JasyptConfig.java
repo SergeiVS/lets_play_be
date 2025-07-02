fb6bbe84554d4048ee4500a218ef0bedb69fdccb
SergeiVS <fam.smirnoff@gmail.com>
2025-07-02 21:21:40 +0200

    Add Jasypt encryption

---
+++ src/main/java/org/lets_play_be/config/JasyptConfig.java
@@ -0,0 +1,28 @@
+package org.lets_play_be.config;
+
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+private final String password = System.getenv("JASYPT_ENCRYPTOR_PASSWORD");
+
+    @Bean(name="jasyptStringEncryptor")
+    public PooledPBEStringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(password);
+        config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+
+    }
+}
