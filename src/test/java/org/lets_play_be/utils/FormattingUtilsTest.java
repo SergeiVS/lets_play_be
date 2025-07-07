c93743597a451c18a3a5a09fa5f0a05392c878e8
SergeiVS <fam.smirnoff@gmail.com>
2025-07-07 21:13:56 +0200

    Refactor Tests

--- src/test/java/org/lets_play_be/utils/FormattingUtilsTest.java
+++ src/test/java/org/lets_play_be/utils/FormattingUtilsTest.java
@@ -1,53 +1,44 @@
 package org.lets_play_be.utils;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.lets_play_be.utils.FormattingUtils.normalizeEmail;
 
 class FormattingUtilsTest {
 
-    @Test
-    void normalizeEmail_Success() {
-        String notNormalizedEmail = " test @ Test. Com ";
+    @ParameterizedTest
+    @ValueSource(strings = {" test @ Test. Com "})
+    void normalizeEmail_Success(String email) {
         String normalizedEmail = "test@test.com";
 
-        assertEquals(normalizedEmail, normalizeEmail(notNormalizedEmail));
-        assertEquals(normalizedEmail, normalizeEmail(normalizedEmail));
+        assertEquals(normalizedEmail, normalizeEmail(email));
     }
 
-    @Test
-    void normalizeEmail_Throws() {
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void normalizeEmail_Throws(String emptyString) {
 
-        assertThrowsExactly(IllegalArgumentException.class, () -> normalizeEmail(""), "Email cannot be null or empty");
-        assertThrowsExactly(IllegalArgumentException.class, () -> normalizeEmail(" "), "Email cannot be null or empty");
+        assertThrowsExactly(IllegalArgumentException.class, () -> normalizeEmail(emptyString), "Email cannot be null or empty");
     }
 
-    @Test
-    void timeStringToOffsetTime() {
+    @ParameterizedTest
+    @ValueSource(strings = {"19:25:30+01:00", "19:25:30-01:00"})
+    void timeStringToOffsetTime_Success(String timeString) {
 
-        String correctTimeString1 = "19:25:30+01:00";
-        String correctTimeString2 = "19:25:30-01:00";
+        var result = FormattingUtils.timeStringToOffsetTime(timeString);
 
-        String incorrectTimeStringHours = "24:00:30+01:00";
-        String incorrectTimeStringMinutes = "00:60:30+01:00";
-        String incorrectTimeStringSeconds = "00:59:60+01:00";
+        assertEquals(timeString.substring(0, 2), String.valueOf(result.getHour()));
+        assertEquals(timeString.substring(3, 5), String.valueOf(result.getMinute()));
+        assertEquals(timeString.substring(6, 8), String.valueOf(result.getSecond()));
+        assertEquals(timeString.substring(8), String.valueOf(result.getOffset()));
 
-        var result1 = FormattingUtils.timeStringToOffsetTime(correctTimeString1);
-        var result2 = FormattingUtils.timeStringToOffsetTime(correctTimeString2);
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"24:00:30+01:00", "00:60:30+01:00", "00:59:60+01:00", "00:59:60+15:00"})
+    void timeStringToOffsetTime_Trows_AssertionError(String timeString) {
 
-        assertEquals(correctTimeString1.substring(0, 2), String.valueOf(result1.getHour()));
-        assertEquals(correctTimeString1.substring(3, 5), String.valueOf(result1.getMinute()));
-        assertEquals(correctTimeString1.substring(6, 8), String.valueOf(result1.getSecond()));
-        assertEquals(correctTimeString1.substring(8), String.valueOf(result1.getOffset()));
-
-        assertEquals(correctTimeString2.substring(0, 2), String.valueOf(result2.getHour()));
-        assertEquals(correctTimeString2.substring(3, 5), String.valueOf(result2.getMinute()));
-        assertEquals(correctTimeString2.substring(6, 8), String.valueOf(result2.getSecond()));
-        assertEquals(correctTimeString2.substring(8), String.valueOf(result2.getOffset()));
-
-        assertThrows(AssertionError.class, () -> FormattingUtils.timeStringToOffsetTime(incorrectTimeStringHours));
-        assertThrows(AssertionError.class, () -> FormattingUtils.timeStringToOffsetTime(incorrectTimeStringMinutes));
-        assertThrows(AssertionError.class, () -> FormattingUtils.timeStringToOffsetTime(incorrectTimeStringSeconds));
+        assertThrows(AssertionError.class, () -> FormattingUtils.timeStringToOffsetTime(timeString));
     }
 }
